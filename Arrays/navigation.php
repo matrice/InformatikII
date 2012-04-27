54ede003c705ced08492d317fac12d8530cd4261
Axel Dürkop <github@trainxl.de>
2012-04-27 15:48:23 +0200

    Aufgabe zu zweistufiger Navigation hinzugefügt

---
+++ Arrays/navigation.php
@@ -0,0 +1,5 @@
+<?php
+
+    // Wie muss ein Array aufgebaut sein, aus dem eine zweistufige Navigation in ungeordneten HTML-Listen
+    // generiert werden soll?
+    // Und wie sieht dann die Schleife aus, mit der das HTML generiert wird?
