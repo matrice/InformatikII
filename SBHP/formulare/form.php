daa2c755bdc8135366cb2845ca7c4d303e43318d
Axel Dürkop <github@trainxl.de>
2012-05-24 12:02:29 +0200

    Beispiel für Login-Formular

---
+++ SBHP/formulare/form.php
@@ -0,0 +1,24 @@
+<?php
+
+    if (isset($_POST['sent']))
+        echo "Gesendet!";
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+
+        <h1>Formularversand mit PHP</h1>
+
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <label for="user">Username:</label>
+            <input type="text" value="" name="user" id="user" />
+            <label for="pass">Password:</label>
+            <input type="text" value="" name="pass" id="pass" />
+            <input type="submit" value="Einloggen" name="sent" />
+        </form>
+    </body>
+</html>
